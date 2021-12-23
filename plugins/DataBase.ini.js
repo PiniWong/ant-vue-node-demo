7a78c2369e96c918b5289e6e33ac37c55354efd7
PiniWong <79297110+PiniWong@users.noreply.github.com>
2021-12-23 09:21:12 +0800

    bug改

--- plugins/DataBase.ini.js
+++ plugins/DataBase.ini.js
@@ -5,5 +5,7 @@ Database.user ='root'
 Database.password = '123456'
 Database.port = 3306
 Database.database = 'pay'//数据库名
+Database.database1 = 'pay'//数据库名
+Database.database2 = 'pay'//数据库名
 
 exports.dbMysql=Database
